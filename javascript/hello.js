fd78783908858b68a071940031716a0e684fdbde
RisabhKedai <58504602+RisabhKedai@users.noreply.github.com>
2020-10-02 23:52:30 +0530

    Create hello.js
    Displaying hello world message in javascript.

---
+++ javascript/hello.js
@@ -0,0 +1,8 @@
+<script>
+// This displays a alert message in the window of browser
+window.alert("Hello World !!");
+// this message shows hello world message in the html of the web page
+document.write("Hello World!!");
+// This displays the message to the console
+console.log("Hello World !!");
+</script>
