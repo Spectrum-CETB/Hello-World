135003f86c7425b69dec34580eb7c3fc4726de7b
Pabodha Wimalasuriya <pabodha.w@evicio.com>
2020-10-10 22:49:23 +0530

    HelloWorld Program in C#

---
+++ C#/helloworld-PBWim.cs
@@ -0,0 +1,17 @@
+namespace ConsoleApp1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+    public class Program
+    {
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, world!");
+			Console.ReadLine();
+		}
+    }
+}
